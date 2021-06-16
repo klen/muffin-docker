7650c1e51d65bcb1dc17486f540d03500b9d2169
Kirill Klenov <horneds@gmail.com>
2021-06-16 15:11:08 +0300

    tests: fix

--- tests/test_base.py
+++ tests/test_base.py
@@ -2,7 +2,7 @@ import requests
 
 
 def test_base(run, pyversion):
-    with run(f"muffin:{pyversion}"):
+    with run(f"horneds/muffin:{pyversion}"):
         res = requests.get('http://127.0.0.1:8000')
         assert res.status_code == 200
         assert 'Hello from Muffin' in res.text
